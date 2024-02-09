8830a199a9ece7f50f3cfee895d6ce3231cd8413
Antonio Marcos Castelo <marcos-12marcos@hotmail.com>
2024-02-09 17:04:21 -0300

    Initial commit

---
+++ release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      { preset: 'conventionalcommits' },
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
+};
