39812cea09fb5628c000dc7adf26c5f14532e712
AMarcosCastelo <marcos-12marcos@hotmail.com>
2024-02-12 13:15:40 -0300

    chore(config): :hammer: create carrot-eslint semantic release config

---
+++ libs/publishale/carrot-eslint/release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  commitPaths: ['libs/publishale/carrot-eslint/*'],
+  extends: 'release.config.js',
+  pkgRoot: 'dist/carrot-eslint',
+  plugins: [
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changeLogFile: 'libs/publishable/carrot-eslint/CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'release(version): Release carrot-eslint ' +
+          // eslint-disable-next-line no-template-curly-in-string
+          '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  // eslint-disable-next-line no-template-curly-in-string
+  tagFormat: 'carrot-eslint@${version}',
+};
--- release.config.base.js
+++ release.config.base.js
@@ -1,5 +1,6 @@
 module.exports = {
   branches: ['main'],
+  extends: 'semantic-release-npm-github-publish',
   plugins: [
     '@semantic-release/commit-analyzer',
     [
