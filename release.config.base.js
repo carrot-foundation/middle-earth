8d23b44f113ead9933f906ef0b774a89a39100d4
AMarcosCastelo <marcos-12marcos@hotmail.com>
2024-07-26 16:45:22 -0300

    fix: remove semantic-release from release.config.base

--- release.config.base.js
+++ release.config.base.js
@@ -1,12 +1,4 @@
 module.exports = {
   branches: ['main'],
   extends: 'semantic-release-npm-github-publish',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    [
-      '@semantic-release/release-notes-generator',
-      { preset: 'conventionalcommits' },
-    ],
-    '@semantic-release/github',
-  ],
 };
